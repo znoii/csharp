fb97c981c2802e94a77a6616c073fc695df900d1
znoii <159401793+znoii@users.noreply.github.com>
2024-09-09 14:32:35 +0700

    Add files via upload

---
+++ polymorfism/polymorfism/Program.cs
@@ -0,0 +1,28 @@
+﻿public class CustomConverter
+{
+    public static bool ConvertTo(string value, out int result)
+    {
+        return int.TryParse(value, out result);
+    }
+
+    public static bool ConvertTo(string value, out double result)
+    {
+        return double.TryParse(value, out result);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        string s = Console.ReadLine();
+        int intValue;
+        CustomConverter.ConvertTo(s, out intValue);
+        Console.WriteLine(intValue);
+
+        string a = Console.ReadLine(); //запятая
+        double doubleValue;
+        CustomConverter.ConvertTo(a, out doubleValue);
+        Console.WriteLine(doubleValue);
+    }
+}
