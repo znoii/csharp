fd336ff320a7733a359e7bf88016055d7175fc89
znoii <159401793+znoii@users.noreply.github.com>
2024-09-23 13:59:04 +0700

    Add files via upload

---
+++ all_singleton/singleton/singleton/Program.cs
@@ -0,0 +1,62 @@
+﻿internal sealed class Servers
+{
+    private static Servers singlet;
+    private List<string> servers = new List<string>();
+
+    private Servers() { }
+
+    internal static Servers Instance
+    { 
+        get { return singlet ?? (singlet = new Servers()); }
+    }
+
+    internal bool AddServer(string server)
+    {
+        if (server.StartsWith("http://", StringComparison.OrdinalIgnoreCase) || server.StartsWith("https://", StringComparison.OrdinalIgnoreCase))
+        {
+            if (!servers.Contains(server))
+            {
+                servers.Add(server);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    internal List<string> GetHttpServers()
+    {
+        return servers.Where(s => s.StartsWith("http://", StringComparison.OrdinalIgnoreCase)).ToList();
+    }
+
+    internal List<string> GetHttpsServers()
+    {
+        return servers.Where(s => s.StartsWith("https://", StringComparison.OrdinalIgnoreCase)).ToList();
+    }
+}
+
+internal class Program
+{
+    internal static void Main()
+    {
+        var servers = Servers.Instance;
+
+        servers.AddServer("http://example_singlt.com");
+        servers.AddServer("https://example_singlt.com");
+        servers.AddServer("http://example2_singlt.com");
+
+        var httpServers = servers.GetHttpServers();
+        var httpsServers = servers.GetHttpsServers();
+
+        Console.WriteLine("HTTP сервера:");
+        foreach (var server in httpServers)
+        {
+            Console.WriteLine(server);
+        }
+
+        Console.WriteLine("HTTPS сервера:");
+        foreach (var server in httpsServers)
+        {
+            Console.WriteLine(server);
+        }
+    }
+}
---
+++ all_singleton/singleton_extra1/singleton_extra1/Program.cs
@@ -0,0 +1,71 @@
+﻿internal sealed class Servers
+{
+    private static Servers singlet;
+    private readonly object lockS = new object();
+    private List<string> servers = new List<string>();
+
+    private Servers() { }
+
+    internal static Servers Instance
+    {
+        get { return singlet ?? (singlet  = new Servers()); }
+    }
+     
+    internal bool AddServer(string server)
+    {
+        lock (lockS)
+        {
+            if (server.StartsWith("http://", StringComparison.OrdinalIgnoreCase) || server.StartsWith("https://", StringComparison.OrdinalIgnoreCase))
+            {
+                if (!servers.Contains(server))
+                {
+                    servers.Add(server);
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    internal List<string> GetHttpServers()
+    {
+        lock (lockS)
+        {
+            return servers.Where(s => s.StartsWith("http://", StringComparison.OrdinalIgnoreCase)).ToList();
+        }
+    }
+
+    internal List<string> GetHttpsServers()
+    {
+        lock (lockS)
+        {
+            return servers.Where(s => s.StartsWith("https://", StringComparison.OrdinalIgnoreCase)).ToList();
+        }
+    }
+}
+internal class Program
+{
+    internal static void Main()
+    {
+        var servers = Servers.Instance;
+
+        servers.AddServer("http://example_singlt.com");
+        servers.AddServer("https://example_singlt.com");
+        servers.AddServer("http://example2_singlt.com");
+
+        var httpServers = servers.GetHttpServers();
+        var httpsServers = servers.GetHttpsServers();
+
+        Console.WriteLine("HTTP сервера:");
+        foreach (var server in httpServers)
+        {
+            Console.WriteLine(server);
+        }
+
+        Console.WriteLine("HTTPS сервера:");
+        foreach (var server in httpsServers)
+        {
+            Console.WriteLine(server);
+        }
+    }
+}
