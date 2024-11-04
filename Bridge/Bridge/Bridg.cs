9b117dcb78ffdd60c0e812e5f59d94421b44114e
znoii <159401793+znoii@users.noreply.github.com>
2024-11-04 08:28:34 +0300

    Add files via upload

---
+++ Bridge/Bridge/Bridg.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    internal class Bridg
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.Write("ввести сообщение для Email: ");
+                string emailMessage = Console.ReadLine();
+
+                Notification emailNotification = new EmailNotification();
+                AbNotification textEmailNotification = new TextNotification(emailNotification);
+                textEmailNotification.SendNotification(emailMessage);
+
+                Console.Write("ввести сообщение для SMS: ");
+                string smsMessage = Console.ReadLine();
+
+                Notification smsNotification = new SMSNotification();
+                AbNotification htmlSmsNotification = new HtmlNotification(smsNotification);
+                htmlSmsNotification.SendNotification(smsMessage);
+
+
+            }
+        }
+    }
+}
---
+++ Bridge/Bridge/Program.cs
@@ -0,0 +1,60 @@
+﻿
+using System;
+
+
+public interface Notification
+{
+    void Send(string message);
+}
+
+public class EmailNotification : Notification
+{
+    public void Send(string message)
+    {
+        Console.WriteLine($"для Email - {message}");
+    }
+}
+
+public class SMSNotification : Notification
+{
+    public void Send(string message)
+    {
+        Console.WriteLine($"для SMS - {message}");
+    }
+}
+public abstract class AbNotification
+{
+    protected Notification _notification;
+
+    protected AbNotification(Notification notification)
+    {
+        _notification = notification;
+    }
+
+    public abstract void SendNotification(string message);
+}
+
+// насл
+public class TextNotification : AbNotification
+{
+    public TextNotification(Notification notification) : base(notification) { }
+
+    public override void SendNotification(string message)
+    {
+        _notification.Send(message);
+    }
+}
+
+public class HtmlNotification : AbNotification
+{
+    public HtmlNotification(Notification notification) : base(notification) { }
+
+    public override void SendNotification(string message)
+    {
+        string htmlMessage = $"<html><body><p>{message}</p></body></html>";
+        _notification.Send(htmlMessage);
+    }
+}
+
+
+
