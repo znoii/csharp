5f1917418a5cb6a513bf953fc34e151ba53ec7b0
znoii <159401793+znoii@users.noreply.github.com>
2024-11-11 14:28:15 +0700

    Add files via upload

---
+++ composite_pattern/composite_pattern/Compose.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace composite_pattern
+{
+    internal class Compose
+    {
+        class Program
+        {
+            static void Main()
+            {
+                Paragraph p1 = new Paragraph("перый параграф");
+                Paragraph p2 = new Paragraph("и второй");
+
+                Section section1 = new Section("ВВЕДЕНИЕ");
+                section1.Add(p1);
+                section1.Add(p2);
+
+                Paragraph p3 = new Paragraph("парагр. второй секции");
+                Section section2 = new Section("ОСН. ЧАСТЬ");
+                section2.Add(p3);
+
+                Document document = new Document();
+                document.Add(section1);
+                document.Add(section2);
+
+                document.Display(0);
+            }
+        }
+    }
+}
---
+++ composite_pattern/composite_pattern/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+
+public interface IDocumentComponent
+{
+    void Add(IDocumentComponent component);
+    void Remove(IDocumentComponent component);
+    void Display(int indentLevel);
+}
+public class Paragraph : IDocumentComponent
+{
+    private string text;
+
+    public Paragraph(string text)
+    {
+        this.text = text;
+    }
+
+    public void Add(IDocumentComponent component)
+    {
+        throw new InvalidOperationException("параграф это простой текст. нельзя добавить");
+    }
+
+    public void Remove(IDocumentComponent component)
+    {
+        throw new InvalidOperationException("параграф это простой текст. нельзя удалить");
+    }
+
+    public void Display(int indentLevel)
+    {
+        Console.WriteLine(new string(' ', indentLevel * 2) + text);
+    }
+}
+public class Section : IDocumentComponent
+{
+    private string title;
+    private List<IDocumentComponent> components = new List<IDocumentComponent>();
+
+    public Section(string title)
+    {
+        this.title = title;
+    }
+
+    public void Add(IDocumentComponent component)
+    {
+        components.Add(component);
+    }
+
+    public void Remove(IDocumentComponent component)
+    {
+        components.Remove(component);
+    }
+
+    public void Display(int indentLevel)
+    {
+        Console.WriteLine(new string(' ', indentLevel * 2) + "Section: " + title);
+        foreach (var component in components)
+        {
+            component.Display(indentLevel + 1);
+        }
+    }
+}
+public class Document : IDocumentComponent
+{
+    private List<IDocumentComponent> sections = new List<IDocumentComponent>();
+
+    public void Add(IDocumentComponent component)
+    {
+        if (component is Section)  
+        {
+            sections.Add(component);
+        }
+        else
+        {
+            throw new InvalidOperationException("нет, только секции");
+        }
+    }
+
+    public void Remove(IDocumentComponent component)
+    {
+        sections.Remove(component);
+    }
+
+    public void Display(int indentLevel)
+    {
+        Console.WriteLine("документ 1");
+        foreach (var section in sections)
+        {
+            section.Display(indentLevel + 1);
+        }
+    }
+}
+
+
