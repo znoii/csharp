b4dea80233f0257da4d4ebe26b246bad40c5f906
znoii <159401793+znoii@users.noreply.github.com>
2024-11-25 10:59:37 +0300

    Add files via upload

---
+++ decorater/decorater/Decorator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace decorater
+{
+    internal class Decorator
+    {
+        class Program
+        {
+            static void Main()
+            {
+                INotifier notifier = new Notifier("pupu@mer.ci.nsu.ru");
+
+                notifier = new SMSNotifier(notifier, "+79139837465");
+                notifier = new FacebookNotifier(notifier, "admin_facebook_account");
+                notifier.Send("@all - общая рассылка");
+                
+            }
+        }
+
+    }
+}
---
+++ decorater/decorater/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+public interface INotifier
+{
+    void Send(string message);
+}
+
+public class Notifier : INotifier
+{
+    private readonly string emailAddress;
+
+    public Notifier(string emailAddress)
+    {
+        this.emailAddress = emailAddress;
+    }
+
+    public virtual void Send(string message)
+    {
+        Console.WriteLine($"По почте {emailAddress}: {message}");           //для всех адм
+    }
+}
+
+public abstract class NotifierDecorator : INotifier
+{
+    protected readonly INotifier notifier;
+
+    public NotifierDecorator(INotifier notifier)
+    {
+        this.notifier = notifier;
+    }
+
+    public virtual void Send(string message)
+    {
+        notifier.Send(message);
+    }
+}
+
+public class SMSNotifier : NotifierDecorator
+{
+    private readonly string phoneNumber;
+
+    public SMSNotifier(INotifier notifier, string phoneNumber) : base(notifier)
+    {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public override void Send(string message)
+    {
+        base.Send(message);
+        Console.WriteLine($"через SMS: {phoneNumber}: {message}");
+    }
+}
+
+public class FacebookNotifier : NotifierDecorator
+{
+    private readonly string facebookAccount;
+
+    public FacebookNotifier(INotifier notifier, string facebookAccount) : base(notifier)
+    {
+        this.facebookAccount = facebookAccount;
+    }
+
+    public override void Send(string message)
+    {
+        base.Send(message);
+        Console.WriteLine($"через Facebook: {facebookAccount}: {message}");
+    }
+}
+
