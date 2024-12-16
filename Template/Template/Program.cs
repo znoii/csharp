86589443a8e093a0981e8ac8727434c0ec1835d1
znoii <159401793+znoii@users.noreply.github.com>
2024-12-16 12:15:42 +0300

    Add files via upload

---
+++ Template/Template/Program.cs
@@ -0,0 +1,67 @@
+﻿class Program
+{
+    static void Main()
+    {
+        ReportGenerator studentReport = new StudentReportGenerator();
+        studentReport.GenerateReport();
+
+        ReportGenerator courseReport = new CourseReportGenerator();
+        courseReport.GenerateReport();
+    }
+}
+
+abstract class ReportGenerator
+{
+    public void GenerateReport()
+    {
+        CollectData();
+        ProcessData();
+        FormatReport();
+        ExportReport();
+    }
+
+    public abstract void CollectData();
+    public abstract void ProcessData();
+    public abstract void FormatReport();
+
+    public void ExportReport()
+    {
+        Console.WriteLine("Report has been exported.");
+    }
+}
+
+class StudentReportGenerator : ReportGenerator
+{
+    public override void CollectData()
+    {
+        Console.WriteLine("collect...");
+    }
+
+    public override void ProcessData()
+    {
+        Console.WriteLine("data...");
+    }
+
+    public override void FormatReport()
+    {
+        Console.WriteLine("report...");
+    }
+}
+
+class CourseReportGenerator : ReportGenerator
+{
+    public override void CollectData()
+    {
+        Console.WriteLine("Collecting...");
+    }
+
+    public override void ProcessData()
+    {
+        Console.WriteLine("processing data...");
+    }
+
+    public override void FormatReport()
+    {
+        Console.WriteLine("formatting report...");
+    }
+}
